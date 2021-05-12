247a0a88271ccc99dd043c4dbd5205c761ee0a8b
13RedFox <sergei181989@gmail.com>
2021-05-12 22:45:51 +0300

    feat: added all files

--- 00-course-test/js/app.js
+++ 00-course-test/js/app.js
@@ -511,4 +511,261 @@
 
 // console.log(`Циклы отработал за ${end - start} миллисекунд`);
 
-// ! Lessons 39 Работа с датами
+// ! Lessons 45 Функции-конструкторы
+
+// function User(name, id) {
+//   this.name = name;
+//   this.id = id;
+//   this.human = true;
+//   this.hello = function () {
+//     console.log(`Hello ${this.name}`);
+//   };
+// }
+
+// User.prototype.exit = function () {
+//   console.log(`User ${this.name} exit`);
+// };
+
+// const ivan = new User('Ivan', 25);
+// const alex = new User('Alex', 20);
+
+// ivan.exit();
+
+// ivan.hello();
+// alex.hello();
+
+// console.log(ivan);
+// console.log(alex);
+
+// ! Lessons 46 Контекст вызова - This
+
+// + 1) Обычная функция: this = window, но если 'use strict' = undefined
+
+// function showThis(a, b) {
+//   console.log(this);
+//   function sum() {
+//     console.log(this);
+//     return a + b;
+//   }
+//   console.log(sum());
+// }
+
+// showThis(4, 5);
+
+// + 2) Контекст у методов объекто - будет сам объект
+
+// const obj = {
+//   a: 20,
+//   b: 13,
+//   sum: function () {
+//     function shout() {
+//       console.log(this);
+//     }
+//     shout();
+//   }
+// };
+
+// obj.sum();
+
+// + 3) this в конструктора и класса - это новый экземпляр объета
+
+// function User(name, id) {
+//   this.name = name;
+//   this.id = id;
+//   this.human = true;
+//   this.hello = function() {
+//     console.log(`Hello ${this.name}`);
+//   }
+// }
+
+// let ivan = new User('Ivan', 23);
+
+// + 4) Ручная привязка this: call, apply, bind
+
+// function sayName(surname) {
+//   console.log(this);
+//   console.log(this.name + surname);
+// }
+
+// const user = {
+//   name: 'John'
+// };
+
+// sayName.call(user, ' Smith');
+// sayName.apply(user, [' Smith']);
+
+// function count(num) {
+//   return this * num;
+// }
+
+// const double = count.bind(2);
+// console.log(double(3));
+// console.log(double(13));
+
+// const btn = document.querySelector('button');
+
+// btn.addEventListener('click', (e) => {
+//   console.log(this);
+//   e.target.style.color = 'crimson';
+// });
+
+// const obj = {
+//   num: 5,
+//   sayNumber: function () {
+//     const say = () => {
+//       console.log(this);
+//     };
+//     say();
+//   }
+// };
+
+// obj.sayNumber();
+
+// const double = (a, b) => a * b;
+
+// console.log(double(4, 5));
+
+// ! 47 Классы (ES6)
+
+// class Rectangle {
+//   constructor(height, width) {
+//     this.height = height;
+//     this.width = width;
+//   }
+
+//   calcArea() {
+//     return this.height * this.width;
+//   }
+// }
+
+// class ColoredRectangleWithText extends Rectangle {
+//   constructor(height, width, text, bgColor) {
+//     super(height, width);
+//     this.text = text;
+//     this.bgColor = bgColor;
+//   }
+
+//   showMyProps() {
+//     console.log(`Text: ${this.text}, color: ${this.bgColor}`);
+//   }
+// }
+
+// const div = new ColoredRectangleWithText(25, 10, 'Hello World', 'red');
+
+// div.showMyProps();
+// console.log(div.calcArea());
+
+// const square = new Rectangle(10, 10);
+// const long = new Rectangle(13, 45);
+
+// console.log(square.calcArea());
+// console.log(long.calcArea());
+
+// ! 49 Rest оператор и параметры по умолчанию(ES6)
+
+// const log = function (a, b, ...rest) {
+//   console.log(a, b, rest);
+// };
+
+// log('basic', 'rest', 'qwerty', 'usage');
+
+// function calcOrDouble(number, basis = 2) {
+//   console.log(number * basis);
+// }
+
+// calcOrDouble(3);
+
+// ! 51 JSON формат передачи данных, глубокое клонирование объектов
+
+// const persone = {
+//   name: 'Alex',
+//   tel: '+7808888554',
+//   parents: {
+//     mom: 'Olga',
+//     dad: 'Mike',
+//   }
+// };
+
+// const clone = JSON.parse(JSON.stringify(persone));
+// clone.parents.mom = 'Ann';
+// console.log(persone);
+// console.log(clone);
+
+// ! 52 AJAX и общение с сервером
+
+// const inputRub = document.querySelector('#rub'),
+//   inputUsd = document.querySelector('#usd');
+
+// inputRub.addEventListener('input', () => {
+//   const request = new XMLHttpRequest();
+
+//   request.open('GET', 'js/current.json');
+//   request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+//   request.send();
+
+//   request.addEventListener('load', () => {
+//     if (request.status === 200) {
+//       const data = JSON.parse(request.response);
+//       inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+//     } else {
+//       inputUsd.value = 'Что-то пошло не так';
+//     }
+//   });
+
+// * status
+// * statusText
+// * response
+// * readyState
+// });
+
+// ! 55 Promise (ES6)
+
+// console.log('Запрос данных...');
+
+// const req = new Promise(function (resolve, reject) {
+//   setTimeout(() => {
+//     console.log('Подготовка данных...');
+
+//     const product = {
+//       name: 'TV',
+//       price: 1000
+//     };
+
+//     resolve(product);
+//   }, 1000);
+// });
+
+// req.then((product) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       product.status = 'order';
+//       resolve(product);
+//     }, 1000);
+//   });
+// }).then(data => {
+//   data.modify = true;
+//   return data;
+// }).then((data) => {
+//   console.log(data);
+// }).catch(() => {
+//   console.error('Произошла ошибка...');
+// }).finally(() => {
+//   console.log('Finally');
+// });
+
+// const test = time => {
+//   return new Promise(resolve => {
+//     setTimeout(() => resolve(), time);
+//   });
+// };
+
+// test(1000).then(() => console.log('1000 ms'));
+// test(2000).then(() => console.log('2000 ms'));
+
+// Promise.all([test(1000), test(2000)]).then(() => {
+//   console.log('All');
+// });
+
+// Promise.race([test(1000), test(2000)]).then(() => {
+//   console.log('Race');
+// });
